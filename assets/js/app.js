02bf1d485fa8ac7499c2b63efa68567b5ea06de9
avinashdahiya <addahiya2002@gmail.com>
2025-03-27 15:52:52 +0530

    root cls chnage

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,46 +2,47 @@
 const navbarbtn = () => {
     document.getElementById("name").classList.toggle("show-navbar")
     document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
     document.getElementById("rotates2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
 
 
 //-----------------------slider-----------------------
-$('.slider-content').slick({
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 700,
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 8,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+// $('.slider-content').slick({
+//     dots: false,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 700,
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 5,
+//                 slidesToScroll: 1,
+//                 infinite: true,
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//     ]
+// });
 
 //----------------------------------------------back to top------------------------------------------------
 const myButton = document.getElementById("myBtn");
